0b4a35a2859376785f490c0180274e249cd1f98a
YakirC0412 <155883698+YakirC0412@users.noreply.github.com>
2024-02-11 19:20:10 +0200

    Tar3

---
+++ HW3/Server/HW1/BL/Flat.cs
@@ -0,0 +1,61 @@
+﻿namespace HW1.BN
+{
+    public class Flat
+    {
+        int id;
+        string city;
+        string address;
+        double price;
+        int numbers_of_rooms;
+        static List<Flat> FlatsList = new List<Flat>();
+
+        public Flat(int id, string city, string address, double price, int numbers_of_rooms)
+        {
+            Id = id;
+            City = city;
+            Address = address;
+            Price = price;
+            Numbers_of_rooms = numbers_of_rooms;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string City { get => city; set => city = value; }
+        public string Address { get => address; set => address = value; }
+        public double Price { get => price; set => price = value; }
+        public int Numbers_of_rooms { get => numbers_of_rooms; set => numbers_of_rooms = value; }
+
+        public Flat() { }
+
+        public bool Insert()
+        {
+            if (!FlatsList.Any(Flat => Flat.Id==Id))
+            {
+                if(numbers_of_rooms>1&&price>100)
+                {
+                    price = 0.9 * price;
+                }
+                FlatsList.Add(this);
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        public static List<Flat> Read() {
+            return FlatsList;
+        }
+
+        public List<Flat> GetFlatByCityAndPrice(string city, double price)
+        {
+            List<Flat> selectedList = new List<Flat>();
+            foreach (Flat f in FlatsList)
+            {
+                if (f.city == city && f.price < price)
+                    selectedList.Add(f);
+            }
+            return selectedList;
+        }
+
+    }
+}
---
+++ HW3/Server/HW1/BL/Vacation.cs
@@ -0,0 +1,68 @@
+﻿
+namespace HW1.BL
+{
+    public class Vacation
+    {
+        int id;
+        int userId;
+        int flatId;
+        DateTime startDate;
+        DateTime endDate;
+        static List<Vacation> vacationsList = new List<Vacation>();
+
+        public Vacation() { }
+
+        public Vacation(int id, int userId, int flatId, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            UserId = userId;
+            FlatId = flatId;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public int FlatId { get => flatId; set => flatId = value; }
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+        public DateTime EndDate { get => endDate; set => endDate = value; }
+
+        public bool Insert()
+        {
+            if (!vacationsList.Any(Vacation => Vacation.id == id))
+            {
+                foreach (Vacation V in vacationsList)
+                {
+                    if (V.flatId == this.flatId)
+                    {
+                        //V.startDate=26.12,V.endDate=30.12 | this_SD=27.12 , this_ED=30.12 --> should retrun true
+                        if (V.startDate <= this.endDate && V.endDate >= this.startDate)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                vacationsList.Add(this);
+                return true;
+            }
+              return false;
+
+        }
+
+        public static List<Vacation> Read()
+        {
+            return vacationsList;
+        }
+
+        public List<Vacation> GetBystartDateAndendDateRuoting(DateTime StartD, DateTime EndD)
+        {
+            List<Vacation> selectedList = new List<Vacation>();
+            foreach (Vacation v in vacationsList)
+            {
+                if (v.startDate >= StartD && v.endDate <= EndD)
+                    selectedList.Add(v);
+            }
+            return selectedList;
+        }
+    }
+}
